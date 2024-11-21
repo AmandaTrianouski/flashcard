da0f368dd50d1141964d4a81379abc2f9054b016
AmandaTrianouski <160858147+AmandaTrianouski@users.noreply.github.com>
2024-11-21 15:55:17 -0300

    .

---  app.js
+++  app.js
@@ -7,10 +7,10 @@ function criaCartao(categoria, pergunta, resposta) {
     <div class="cartao__conteudo">
     <h3>${categoria}</h3>
     <div class="cartao__conteudo__pergunta">
-    <p>${pergunta}</p>
+        <p>${pergunta}</p>
     </div>
     <div class="cartao__conteudo__resposta">
-    <p>${resposta}</p>
+        <p>${resposta}</p>
     </div>
     </div>
     `
@@ -21,8 +21,8 @@ function criaCartao(categoria, pergunta, resposta) {
         respostaEstaVisivel = !respostaEstaVisivel
         cartao.classList.toggle('active', respostaEstaVisivel)
     }
-    
     cartao.addEventListener('click', viraCartao)
+
 
     container.appendChild(cartao)
 
